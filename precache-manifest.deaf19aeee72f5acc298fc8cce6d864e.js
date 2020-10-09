fc191c7aa3d3c78374e5aae5e5435f29451dd65c
iasjem <jemimah.beryl.sai@gmail.com>
2020-10-09 12:08:36 +0800

    ready for deployment

---
+++ precache-manifest.deaf19aeee72f5acc298fc8cce6d864e.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7646f17aa4cc7271bc921a6fcf98a3e1",
+    "url": "/test_deploy_orbis-web-app/index.html"
+  },
+  {
+    "revision": "3440b70d1f734cb60a37",
+    "url": "/test_deploy_orbis-web-app/static/css/3.a4deadbc.chunk.css"
+  },
+  {
+    "revision": "7424a34e6dcbc8927260",
+    "url": "/test_deploy_orbis-web-app/static/css/main.9e8d0c6d.chunk.css"
+  },
+  {
+    "revision": "3440b70d1f734cb60a37",
+    "url": "/test_deploy_orbis-web-app/static/js/3.975b2454.chunk.js"
+  },
+  {
+    "revision": "beb87b724ab3daba74137a0ccc8effa0",
+    "url": "/test_deploy_orbis-web-app/static/js/3.975b2454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc7e69a88c8d0b0d619",
+    "url": "/test_deploy_orbis-web-app/static/js/4.5b0149ce.chunk.js"
+  },
+  {
+    "revision": "31ba93c955f03c89fba5",
+    "url": "/test_deploy_orbis-web-app/static/js/5.c6a3d2f9.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/test_deploy_orbis-web-app/static/js/5.c6a3d2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32f0318aeedd9393ebf9",
+    "url": "/test_deploy_orbis-web-app/static/js/6.955a1f84.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/test_deploy_orbis-web-app/static/js/6.955a1f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7424a34e6dcbc8927260",
+    "url": "/test_deploy_orbis-web-app/static/js/main.4f17cb31.chunk.js"
+  },
+  {
+    "revision": "fabe31378dff776d3bff",
+    "url": "/test_deploy_orbis-web-app/static/js/runtime-main.e168572e.js"
+  },
+  {
+    "revision": "7394214b64fb7ca07ea6",
+    "url": "/test_deploy_orbis-web-app/static/js/xlsx.9260c80b.chunk.js"
+  },
+  {
+    "revision": "6d551d57ec8710d131ac5933b5296c2e",
+    "url": "/test_deploy_orbis-web-app/static/media/add.6d551d57.svg"
+  },
+  {
+    "revision": "217151821134423891888ddb33621a5e",
+    "url": "/test_deploy_orbis-web-app/static/media/atomnew.21715182.svg"
+  },
+  {
+    "revision": "44c76c753a73342fa33cec9eab550982",
+    "url": "/test_deploy_orbis-web-app/static/media/comment_bubble.44c76c75.svg"
+  },
+  {
+    "revision": "2e391d6c26a66288f62bfd2f9c005005",
+    "url": "/test_deploy_orbis-web-app/static/media/comment_glasses.2e391d6c.svg"
+  },
+  {
+    "revision": "9b612b87590c71c24eccbd169cf76894",
+    "url": "/test_deploy_orbis-web-app/static/media/comment_paperclip.9b612b87.svg"
+  },
+  {
+    "revision": "e0536c4925e5d64a9d7074da93ec5388",
+    "url": "/test_deploy_orbis-web-app/static/media/dummy-image.e0536c49.png"
+  },
+  {
+    "revision": "a8f4e7c64a35cfa7f4e30610310a0005",
+    "url": "/test_deploy_orbis-web-app/static/media/flare.a8f4e7c6.svg"
+  },
+  {
+    "revision": "18566529d769b352ca266aabe63191a1",
+    "url": "/test_deploy_orbis-web-app/static/media/hamburger.18566529.svg"
+  },
+  {
+    "revision": "f4b3599b5433791fe3638fecd24ebd1a",
+    "url": "/test_deploy_orbis-web-app/static/media/info.f4b3599b.svg"
+  },
+  {
+    "revision": "c01f1613cc7050131e95a1c6441defb5",
+    "url": "/test_deploy_orbis-web-app/static/media/logout.c01f1613.svg"
+  },
+  {
+    "revision": "580b9bc9ce8f36172af36f7913cc53de",
+    "url": "/test_deploy_orbis-web-app/static/media/notification.580b9bc9.svg"
+  },
+  {
+    "revision": "a30d02135e1e8bff8e59cb5a920f00aa",
+    "url": "/test_deploy_orbis-web-app/static/media/orbis-logo-light.a30d0213.svg"
+  },
+  {
+    "revision": "c593cc6be6798e6d4505540b9a4311d5",
+    "url": "/test_deploy_orbis-web-app/static/media/pattern-bg.c593cc6b.svg"
+  },
+  {
+    "revision": "fa743c81c6a56a8159fcaf6ed85602e0",
+    "url": "/test_deploy_orbis-web-app/static/media/plane.fa743c81.svg"
+  },
+  {
+    "revision": "ddacc0c76083684e1bf9e94558fd1f9d",
+    "url": "/test_deploy_orbis-web-app/static/media/quincy.ddacc0c7.jpeg"
+  },
+  {
+    "revision": "b2feb1c702b9b4c46343cfc222770c0d",
+    "url": "/test_deploy_orbis-web-app/static/media/search.b2feb1c7.svg"
+  },
+  {
+    "revision": "cc1c1864648322494ce7f148b287e6e0",
+    "url": "/test_deploy_orbis-web-app/static/media/settings.cc1c1864.svg"
+  },
+  {
+    "revision": "efa6f5f36667bd8b6c857206bd60557a",
+    "url": "/test_deploy_orbis-web-app/static/media/shutter.efa6f5f3.svg"
+  }
+]);
---
+++ static/js/xlsx.9260c80b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpatom-analytics-portal-app"]=this["webpackJsonpatom-analytics-portal-app"]||[]).push([[2],{511:function(a,p){},600:function(a,p){},601:function(a,p){}}]);
+//# sourceMappingURL=xlsx.9260c80b.chunk.js.map
